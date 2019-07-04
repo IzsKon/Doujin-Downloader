de6d29290a69bd19338f8243be65cbef83d1e9e7
IzsKon <eric246871@gmail.com>
2019-07-04 22:20:29 +0800

    Add download tasks queue
    Cannot cancel download task for now.
    The UI is imcomplete.

--- Doujin/Form1.Designer.cs
+++ Doujin/Form1.Designer.cs
@@ -28,59 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.magicNumber = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// magicNumber
-			// 
-			this.magicNumber.Font = new System.Drawing.Font("PMingLiU", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-			this.magicNumber.ImeMode = System.Windows.Forms.ImeMode.Alpha;
-			this.magicNumber.Location = new System.Drawing.Point(9, 28);
-			this.magicNumber.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-			this.magicNumber.Name = "magicNumber";
-			this.magicNumber.Size = new System.Drawing.Size(104, 33);
-			this.magicNumber.TabIndex = 0;
-			this.magicNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.magicNumber_KeyPress);
-			this.magicNumber.MouseDown += new System.Windows.Forms.MouseEventHandler(this.magicNumber_MouseDown);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.BackColor = System.Drawing.Color.Transparent;
-			this.label1.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-			this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-			this.label1.Location = new System.Drawing.Point(7, 9);
-			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(98, 16);
-			this.label1.TabIndex = 2;
-			this.label1.Text = "magic number";
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-			this.ClientSize = new System.Drawing.Size(120, 69);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.magicNumber);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "Form1";
-			this.Text = "Doujin";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.magicNumTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.doujinCoverPic = new System.Windows.Forms.PictureBox();
+            this.doujinTitleLabel = new System.Windows.Forms.Label();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.doujinLenLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.doujinCoverPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // magicNumTextBox
+            // 
+            this.magicNumTextBox.Font = new System.Drawing.Font("PMingLiU", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.magicNumTextBox.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.magicNumTextBox.Location = new System.Drawing.Point(12, 35);
+            this.magicNumTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.magicNumTextBox.Name = "magicNumTextBox";
+            this.magicNumTextBox.Size = new System.Drawing.Size(137, 40);
+            this.magicNumTextBox.TabIndex = 0;
+            this.magicNumTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.magicNumber_KeyPress);
+            this.magicNumTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.magicNumber_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "magic number";
+            // 
+            // doujinCoverPic
+            // 
+            this.doujinCoverPic.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.doujinCoverPic.Location = new System.Drawing.Point(13, 83);
+            this.doujinCoverPic.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.doujinCoverPic.Name = "doujinCoverPic";
+            this.doujinCoverPic.Size = new System.Drawing.Size(391, 319);
+            this.doujinCoverPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.doujinCoverPic.TabIndex = 3;
+            this.doujinCoverPic.TabStop = false;
+            // 
+            // doujinTitleLabel
+            // 
+            this.doujinTitleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.doujinTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.doujinTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.doujinTitleLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.doujinTitleLabel.Location = new System.Drawing.Point(11, 406);
+            this.doujinTitleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.doujinTitleLabel.Name = "doujinTitleLabel";
+            this.doujinTitleLabel.Size = new System.Drawing.Size(393, 141);
+            this.doujinTitleLabel.TabIndex = 4;
+            this.doujinTitleLabel.Text = "Titile";
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.downloadButton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.downloadButton.FlatAppearance.BorderSize = 0;
+            this.downloadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.downloadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.downloadButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.downloadButton.Location = new System.Drawing.Point(154, 31);
+            this.downloadButton.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(151, 44);
+            this.downloadButton.TabIndex = 5;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = false;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // doujinLenLabel
+            // 
+            this.doujinLenLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.doujinLenLabel.AutoSize = true;
+            this.doujinLenLabel.BackColor = System.Drawing.Color.Transparent;
+            this.doujinLenLabel.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.doujinLenLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.doujinLenLabel.Location = new System.Drawing.Point(12, 547);
+            this.doujinLenLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.doujinLenLabel.Name = "doujinLenLabel";
+            this.doujinLenLabel.Size = new System.Drawing.Size(50, 20);
+            this.doujinLenLabel.TabIndex = 6;
+            this.doujinLenLabel.Text = "pages";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(982, 576);
+            this.Controls.Add(this.doujinLenLabel);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.doujinTitleLabel);
+            this.Controls.Add(this.doujinCoverPic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.magicNumTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Doujin";
+            ((System.ComponentModel.ISupportInitialize)(this.doujinCoverPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox magicNumber;
+        private System.Windows.Forms.TextBox magicNumTextBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox doujinCoverPic;
+        private System.Windows.Forms.Label doujinTitleLabel;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.Label doujinLenLabel;
     }
 }
 
--- Doujin/Form1.cs
+++ Doujin/Form1.cs
@@ -10,71 +10,211 @@ using System.Windows.Forms;
 using System.Net;
 using System.IO;
 using System.Threading;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Doujin
 {
-	public partial class Form1 : Form
-	{
-		private bool selected = false;
-		private LinkedList<Form2> downloadPages = new LinkedList<Form2>();
-		private int currentDownload = 0;
-		private const int maxDownload = 3;
+    public partial class Form1 : Form
+    {
+        private string doujinTitle = "";
+        private int doujinLen = 0;
+        private CommonOpenFileDialog folderSelectDialog;
+        private CancellationTokenSource tokenSource;
 
-		public Form1()
-		{
-			InitializeComponent();
-			magicNumber.Focus();
-		}
+        private bool selected = false;
+        private LinkedList<Task> downloadTasks = new LinkedList<Task>();
 
-		private async void magicNumber_KeyPress(object sender, KeyPressEventArgs e)
-		{
-			if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-				e.Handled = true;
-			if (e.KeyChar == (char)13)
-			{
-				selected = true;
-				magicNumber.SelectAll();
+        public Form1()
+        {
+            InitializeComponent();
+            magicNumTextBox.Focus();
 
-				// set up download page
-				var downloadPage = new Form2(magicNumber.Text);
-				downloadPage.checkMaxReached = () =>
-				{
-					return currentDownload >= maxDownload;
-				};
-				downloadPage.startDownloadEvent += (sen, eve) =>
-				{
-					currentDownload++;
-				};
-				downloadPage.finishEvent += (sen, eve) =>
-				{
-					currentDownload--;
-					downloadPages.Remove(downloadPage);
-				};
-				// add to list
-				downloadPages.AddLast(downloadPage);
+            folderSelectDialog = new CommonOpenFileDialog();
+            folderSelectDialog.IsFolderPicker = true;
+        }
 
-				// set up task
-				var mainTask = new Task(() =>
-				{
-                    downloadPage.load();
-				});
-				mainTask.Start();
-				await mainTask; // asynchronouly wait page to load
+        private async void magicNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+            if (e.KeyChar == (char)13)
+            {
+                selected = true;
+                magicNumTextBox.SelectAll();
 
-                // finish loading, show page
+                // set up task
+                var mainTask = new Task(() =>
+                {
+                    load();
+                });
+                mainTask.Start();
+                await mainTask; // asynchronouly wait page to load
+
+            }
+        }
+
+        private void magicNumber_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!selected) magicNumTextBox.SelectAll();
+            else magicNumTextBox.DeselectAll();
+            selected = !selected;
+        }
+
+        public void load()
+        {
+            // get doujin
+            WebRequest doujinPageRequest;
+            string doujinPage = "";
+            try
+            {
+                doujinPageRequest = WebRequest.Create(@"https://nhentai.net/g/" + magicNumTextBox.Text + '/');
+                doujinPageRequest.Timeout = 10000;
+                doujinPageRequest.Method = "GET";
+                doujinPage = new StreamReader(doujinPageRequest.GetResponse().GetResponseStream()).ReadToEnd();
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Cannot find page.\nPlease check your Internet and make sure the number you enter is valid.",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                return;
+            }
+
+            try
+            {
+                // doujin title
+                doujinTitleLabel.Invoke((Action)(() =>
+                {
+                    int titleStart = doujinPage.IndexOf("<h2>") + "<h2>".Length;
+                    int titleLength = doujinPage.IndexOf("</h2>") - titleStart;
+                    doujinTitle = doujinPage.Substring(titleStart, titleLength);
+                    doujinTitleLabel.Text = doujinTitle;
+                }));
+
+                // doujin length
+                doujinLenLabel.Invoke((Action)(() =>
+                {
+                    int lengthStart = doujinPage.IndexOf("<div>") + "<div>".Length;
+                    int lengthLength = doujinPage.IndexOf(" pages</div>") - lengthStart;
+                    doujinLen = int.Parse(doujinPage.Substring(lengthStart, lengthLength));
+                    doujinLenLabel.Text = doujinLen.ToString() + " pages";
+                }));
+
+                // doujin cover
+                doujinCoverPic.Invoke((Action)(() =>
+                {
+                    doujinPage = doujinPage.Substring(doujinPage.IndexOf("<meta itemprop=\"image\" content=\"") + "<meta itemprop=\"image\" content=\"".Length);
+                    doujinPage = doujinPage.Substring(0, doujinPage.IndexOf("\""));
+                    doujinPageRequest = WebRequest.Create(doujinPage);
+                    doujinPageRequest.Timeout = 10000;
+                    doujinPageRequest.Method = "GET";
+                    doujinCoverPic.Image = Image.FromStream(doujinPageRequest.GetResponse().GetResponseStream());
+                }));
+            }
+            catch (InvalidOperationException)
+            {
+                // window closed before task cancel,
+                // causing progressBar1 to be a invalid object to operate.
+                // This exception is inevitable but not a critical issue,
+                // just let the exception go, task will be canceled soon.
+            }
+        }
+
+        private async void downloadButton_Click(object sender, EventArgs e)
+        {
+            // create folder
+            string path = "";
+            if (folderSelectDialog.ShowDialog() != CommonFileDialogResult.Ok) return;
+            path = folderSelectDialog.FileName + "\\" + doujinTitle;
+            System.Diagnostics.Debug.WriteLine(path);
+            if (Directory.Exists(path))
+            {
+                var result = MessageBox.Show("Folder already exist!\nReplace Folder?", "Folder already exist!",
+                                             MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (result == DialogResult.No) return;
+            }
+            else Directory.CreateDirectory(path);
+
+            // disable download button
+            //downloadButton.Enabled = false;
+
+            // emit start event
+            //startDownloadEvent?.Invoke(null, null);
+
+            // set this task cancellable
+            tokenSource = new CancellationTokenSource();
+            Task downloadTask = new Task(() =>
+            {
+                downloadDoujin(path, tokenSource.Token, magicNumTextBox.Text);
+            });
+            downloadTasks.AddLast(downloadTask);
+            if(downloadTasks.Count == 1)
+            {
+                downloadTask.Start();
+                await downloadTask;
+            }
+
+            // canceled?
+            if (tokenSource.IsCancellationRequested)
+                MessageBox.Show("Download canceled", "Notation", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private async void downloadDoujin(string path, CancellationToken cts, string magicNum)
+        {
+            WebRequest hentaiPage;
+            Random ran = new Random(int.Parse(magicNumTextBox.Text));
+            string imageLink = "";
+            for (int i = 1; i <= doujinLen; i++)
+            {
+                // if task is ordered to cancel
+                if (cts.IsCancellationRequested) return;
+
+                //download delay. DO NOT REMOVE!!
+                Thread.Sleep(ran.Next(500, 1000));
+
+                // go to the hentai page
                 try
                 {
-				    downloadPage.Show();
+                    hentaiPage = WebRequest.Create(@"https://nhentai.net/g/" + magicNum + "/" + i.ToString());
+                    hentaiPage.Timeout = 10000;
+                    hentaiPage.Method = "GET";
+                    imageLink = new StreamReader(hentaiPage.GetResponse().GetResponseStream()).ReadToEnd();
                 }
-                catch(System.ObjectDisposedException){}
-			}
-		}
+                catch (WebException) { break; }
 
-		private void magicNumber_MouseDown(object sender, MouseEventArgs e)
-		{
-			if (!selected) magicNumber.SelectAll();
-			else magicNumber.DeselectAll();
-			selected = !selected;
-		}
-	}
+                // find the image link
+                imageLink = imageLink.Substring(imageLink.LastIndexOf("<img src=\"") + "<img src = ".Length - 1);
+                imageLink = imageLink.Substring(0, imageLink.IndexOf("\""));
+
+                // download image
+                try
+                {
+                    WebClient mywebclient = new WebClient();
+                    mywebclient.DownloadFile(imageLink, path + "\\" + i.ToString() + ".jpg");
+                }
+                catch (WebException)
+                {
+                    var result = MessageBox.Show("We have problem downloading " + magicNum + ", page" + i.ToString(),
+                        "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+
+                    // retry?
+                    if (result == DialogResult.Retry)
+                    {
+                        --i;
+                        continue;
+                    }
+                    else return;
+                }
+            }
+            downloadTasks.RemoveFirst();
+
+            // download next doujin
+            if(downloadTasks.Count >= 0)
+            {
+                downloadTasks.ElementAt(0).Start();
+                await downloadTasks.ElementAt(0);
+            }
+        }
+
+    }
 }
